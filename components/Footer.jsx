b27191d8afe7d53abdf6d1614f0fe77e6a0c97d3
Jest13 <camil.hirane@ynov.com>
2022-11-28 23:49:05 +0100

    footer complet

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import Image from "next/image";
+import { RiSendPlaneFill} from "react-icons/ri";
+import { TiSocialTwitter,
+         TiSocialYoutube,
+         TiSocialLinkedin,
+         TiSocialFacebook,
+         TiSocialInstagram
+        } from "react-icons/all";
+
+// IMPORT INTERN
+import Style from '../styles/Footer.module.css';
+import logo from '../img/ethereumm.png'
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
+
+            <div className={Style.footer}>
+                <div className={Style.footer__box}>
+                    <Image src={logo} alt={logo} width={100} height={100}/>
+                </div>
+
+                <div className={Style.footer__box}>
+                    <div className={Style.footer__input}>
+                        <input type="email" placeholder="email"/>
+                        <RiSendPlaneFill/>
+                    </div>
+                </div>
+
+                <div className={Style.footer__box}>
+                    <div className={Style.social}>
+                        <TiSocialInstagram/>
+                        <TiSocialTwitter/>
+                        <TiSocialYoutube/>
+                        <TiSocialLinkedin/>
+                        <TiSocialFacebook/>
+                    </div>
+                </div>
+
+            </div>
+
     )
 };
 
