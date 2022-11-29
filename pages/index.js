f3bb822eab0e7016be77ef914fc9ee24d8a2a349
Jest13 <camil.hirane@ynov.com>
2022-11-29 22:21:23 +0100

    index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
-import React from 'react'
+import React,{useEffect, useState, useContext} from 'react'
+import { useRouter} from "next/router";
+import Image from "next/image";
+import { ethers } from "ethers";
+import Link from "next/link";
+import { SiMinutemailer} from "react-icons/all";
+
+// IMPORT INTERNE
+
+import Style from "../styles/index.modul.css";
+import ethereumm from "../img/ethereumm.png";
 
 const index = () => {
   return (
